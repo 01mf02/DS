1fb4a65f7231c66dec19599fc5c6f980d6e1a03c
Alex <alex@alexlaptop.(none)>
2012-04-13 11:10:06 +0200

    port put into node

--- 03/Node.java
+++ 03/Node.java
@@ -1,19 +1,26 @@
 class Node {
 	private final String address;
+	private final int port;
 	private int age;
 
-	public Node(String address) {
+	public Node(String address, int port) {
 		this.address = address;
+		this.port = port;
 		this.age = 0;
 	}
 
-	public Node(String address, int age) {
+	public Node(String address, int port, int age) {
 		this.address = address;
+		this.port = port;
 		this.age = age;
 	}
 
 	public String getAddress() {
 		return this.address;
+	}
+
+	public int getPort() {
+		return this.port;
 	}
 
 	public int getAge() {
@@ -24,9 +31,12 @@ class Node {
 		this.age++;
 	}
 
-	public boolean compareAddress(Node node) {
+	public boolean compareAddressAndPort(Node node) {
 		if (this.address.equals(node.getAddress())) {
-			return true;
+			if (this.port == node.getPort()) {
+				return true;
+			}
+			return false;
 		} else {
 			return false;
 		}
